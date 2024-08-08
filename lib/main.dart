94bda8b57d264dba349ce3d0ea3bff5b2b40bd1d
Daniel Augusto <danielrgb2@gmail.com>
2024-08-08 20:21:14 -0300

    Feat: criando multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ilia_movies/dependency_injection/injector.dart';
+import 'package:ilia_movies/presentation/home_screen/bloc/movie_now_playing/movie_now_playing_bloc.dart';
+import 'package:ilia_movies/presentation/home_screen/bloc/movie_now_playing/movie_now_playing_event.dart';
+import 'package:ilia_movies/presentation/home_screen/bloc/popular_movies/popular_movies_bloc.dart';
+import 'package:ilia_movies/presentation/home_screen/bloc/popular_movies/popular_movies_event.dart';
 import 'package:ilia_movies/presentation/home_screen/home_screen.dart';
 
 void main() async {
@@ -10,9 +16,20 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final getIt = GetIt.instance;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MovieNowPlayingBloc>(
+          create: (context) => getIt<MovieNowPlayingBloc>()..add(FetchMoviesNowPlaying()),
+        ),
+        BlocProvider<PopularMoviesBloc>(
+          create: (context) => getIt<PopularMoviesBloc>()..add(FetchPopularMovies()),
+        ),
+        // Adicione mais BlocProviders conforme necessário
+      ],
+      child: MaterialApp(
       title: 'Ília Movies',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -20,6 +37,7 @@ class MyApp extends StatelessWidget {
       ),
       home:
           HomeScreen(), // Uma tela inicial onde você pode navegar para as outras
+    ),
     );
   }
 }
